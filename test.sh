fc7b8e0e2cc95b82a48610fb2e5e3b0b37a3d1b6
zrma <bulbitain@gmail.com>
2019-08-31 00:28:55 +0900

    Add valgrind memcheck step to test script

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ cmake --version
 cmake CMakeLists.txt
 cmake -Dtest=ON .
 make Google_Tests_run
-./test/Google_Tests_run
+valgrind --leak-check=full --show-leak-kinds=all --leak-resolution=med --track-origins=yes --vgdb=no ./test/Google_Tests_run
 make clean
 rm ./CMakeCache.txt
