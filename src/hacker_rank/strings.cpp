843f911fa6475a2150fa2ccae0d6c8d85f8bcb2c
zrma <bulbitain@gmail.com>
2019-08-31 18:00:17 +0900

    Do refactoring: change loop end condition

--- src/hacker_rank/strings.cpp
+++ src/hacker_rank/strings.cpp
@@ -6,16 +6,14 @@
 
 std::vector<int> parse_int(const std::string &str) {
     auto result = std::vector<int>();
-    const auto delimiter = std::string(",");
-    size_t start = 0, pos = 0;
-    while ((pos = str.find(delimiter, start)) != std::string::npos) {
+    const std::string delimiter(",");
+    size_t start = 0;
+
+    while (start < str.size()) {
+        const auto pos = std::min(str.find(delimiter, start), str.size());
         const auto token = str.substr(start, pos - start);
         result.push_back(std::strtol(token.c_str(), nullptr, 10));
         start += token.size() + delimiter.length();
-    }
-    if (str.size() - start > 0) {
-        const auto token = str.substr(start, str.size() - start);
-        result.push_back(std::strtol(token.c_str(), nullptr, 10));
     }
     return result;
 }
